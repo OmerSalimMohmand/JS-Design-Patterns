86c7c0549dffe3f47254486726ed72c09e0c6bd4
omersalim.tech@gmail.com <omersalim.tech@gmail.com>
2025-12-28 23:56:32 -0700

    Reviewed upto observer pattern

--- 4-observer-pattern/main.js
+++ 4-observer-pattern/main.js
@@ -1,6 +1,6 @@
 class Subject {
   constructor() {
-    this.observers = []; // This array stores all the subscriber methods that needs to be called/nofied when Subject receives the data
+    this.observers = []; // This array stores all the subscriber methods that needs to be called/notified when Subject receives the data
   }
 
   subscribe(observer) {
